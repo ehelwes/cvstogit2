ba140ede06f63d9877e96ba5a390833a3632eda3
ovew <ovew>
2006-08-24 08:56:34 +0000

    knavet1.2 051220

---
+++ fetchpersonnavet.sql
@@ -0,0 +1,453 @@
+drop procedure fetchpersonnavet;
+create procedure fetchpersonnavet(
+       p_persnr varchar(12))
+	RETURNING
+	INTEGER,	--p_svar			1 
+	--tbasperson
+	char(1),	--p_arskyddad			2
+	varchar(12),	--p_persnr			3
+	--tKyrkoperson                                  
+	VARCHAR(80),	--p_fornamn			4                     
+	VARCHAR(80),	--p_tilltalsnamn		5                
+	VARCHAR(60),	--p_efternamn           	6
+	VARCHAR(40),	--p_mellannamn			7        
+	VARCHAR(36),	--p_aviseringsnamn		8
+	VARCHAR(35),	--p_fastighet			9
+	CHAR(2),	--p_avregistreringsorsak	10
+	CHAR(6),	--p_fblkf			11	
+	CHAR(6),	--p_kyrkolkf			12
+	DATE,		--p_kyrkotillhorighetsdatum	13
+	CHAR(3),	--p_medlemstyp			14
+	DATE,		--p_andraddatum			15
+	-- tadress                                      
+	CHAR(2),	--p_adresstyp                   16
+	VARCHAR(35),	--p_coadress                    17	
+	VARCHAR(35),	--p_adress1                     18
+	VARCHAR(35),	--p_adress2                     19
+	VARCHAR(35),	--p_adress3                     20
+	VARCHAR(9),	--p_postnummer                  21
+	VARCHAR(35),	--p_postort                     22
+	VARCHAR(35),	--p_land                        23
+	varchar(2),	--p_civilstand			24 nytt 20040402 HW
+	DATE,		--p_civilstandsdatum		25 nytt 20040402 HW
+	CHAR(1),	--p_status			26 nytt 20040402 HW
+	varchar(8);	--p_avregisteringsdatum		27 nytt 20050215 Perre
+
+--@(#)$Id: fetchpersonnavet.sql,v 1.1 2006/08/24 08:56:34 ovew Exp $
+
+	                                                                     
+-- Skapat av: Henric wollert                    
+-- Datum: 2003-10-02                            
+-- Version: 1                                   
+-- Rutinbeteckning ??                           
+-- Beskr: SP för person och adress uppgifter åt knavet 
+-- Tabeller: tbasperson, tkyrkoperson, tadress.
+-- Sökbegr: Personnummer.
+-- Retur: 23 parametrar.
+
+--Returparametrar
+DEFINE p_svar				INTEGER;		--1  
+DEFINE p_arskyddad			char(1);		--2  
+DEFINE p_persnr_ut			varchar(12);    	--3  
+DEFINE p_fornamn			VARCHAR(80);    	--4  
+DEFINE p_tilltalsnamn			VARCHAR(80);    	--5  
+DEFINE p_efternamn           		VARCHAR(60);    	--6  
+DEFINE p_mellannamn			VARCHAR(40);    	--7  
+DEFINE p_aviseringsnamn			VARCHAR(36);    	--8  
+DEFINE p_fastighet			VARCHAR(35);    	--9  
+DEFINE p_avregistreringsorsak		CHAR(2);        	--10 
+DEFINE p_fblkf				CHAR(6);		--11	
+DEFINE p_kyrkolkf			CHAR(6);		--12 
+DEFINE p_kyrkotillhorighetsdatum	DATE;	        	--13 
+DEFINE p_medlemstyp			CHAR(3);		--14 
+DEFINE p_andraddatum			DATE;	        	--15 
+DEFINE p_adresstyp              	CHAR(2);	     	--16 
+DEFINE p_coadress               	VARCHAR(35);         	--17	
+DEFINE p_adress1                	VARCHAR(35);         	--18 
+DEFINE p_adress2                	VARCHAR(35);         	--19 
+DEFINE p_adress3                	VARCHAR(35);         	--20 
+DEFINE p_postnummer             	VARCHAR(9);          	--21 
+DEFINE p_postort                	VARCHAR(35);         	--22 
+DEFINE p_land                   	VARCHAR(35);         	--23 
+DEFINE p_civilstand			varchar(2); 		--24 nytt 20040402 HW
+DEFINE p_civilstandsdatum		DATE;			--25 nytt 20040402 HW
+DEFINE p_status				CHAR(1);		--26 nytt 20040402 HW
+DEFINE p_avregisteringsdatum		VarCHAR(8);		--27 nytt 20050215 Perre
+DEFINE p_kontroll			INTEGER;		-- 20051216 Perre			
+	
+--tempvariabler
+DEFINE p_personid			INTEGER;
+DEFINE p_skapaddatum			DATE;	
+DEFINE p_sortnr				INTEGER;
+
+-- Felrutin                   
+DEFINE sql_err integer;       
+DEFINE isam_err integer;      
+DEFINE error_info char(70);   
+
+ON exception set sql_err, isam_err, error_info
+	call error_log(sql_err, isam_err, error_info,'fetchpersonnavet',3061);
+	return       3061, -- SQL fel i fetchpersonnavet.
+	" ",	--2 	
+	p_persnr,--3         
+	" ",    --4         
+	" ",    --5         
+	" ",    --6         
+	" ",    --7         
+	" ",    --8           
+	" ",    --9         
+	" ",    --10        
+	" ",    --11        
+	" ",    --12        
+	" ",    --13        
+	" ",    --14        
+	" ",    --15        
+	" ",    --16          
+	" ",    --17        
+	" ",    --18        
+	" ",    --19          
+	" ",    --20        
+	" ",    --21        
+	" ",    --22        
+	" ",  	--23
+	" ",	--24
+	NULL,	--25
+	" ",	--26                                
+	NULL;	--27                                
+	                
+	raise exception sql_err, isam_err, error_info;
+END exception;
+
+SET LOCK MODE TO WAIT 10;
+
+--Returparametrar
+LET p_svar			=0;
+LET p_arskyddad			=" ";
+
+LET p_fornamn			=" ";
+LET p_tilltalsnamn		=" ";	
+LET p_efternamn           	=" ";
+LET p_mellannamn		=" ";
+LET p_aviseringsnamn		=" ";
+LET p_fastighet			=" ";
+LET p_avregistreringsorsak	=" ";
+LET p_fblkf			=" ";
+LET p_kyrkolkf			=" ";
+LET p_kyrkotillhorighetsdatum	=" ";
+LET p_medlemstyp		=" ";
+LET p_andraddatum		=" ";
+LET p_adresstyp              	=" ";
+LET p_coadress               	=" ";
+LET p_adress1                	=" ";
+LET p_adress2                	=" ";
+LET p_adress3                	=" ";
+LET p_postnummer             	=" ";
+LET p_postort                	=" ";
+LET p_land                   	=" ";
+LET p_civilstandsdatum		=NULL;
+LET p_civilstand		=" ";	
+LET p_avregisteringsdatum       =NULL; -- Perre 20050215
+
+--tempvariabler
+LET p_personid			=0;
+LET p_skapaddatum		=" ";	
+LET p_sortnr			=0;
+LET p_kontroll = 0;
+
+LET p_persnr_ut=p_persnr;			
+
+
+SELECT	bp.arskyddad,			
+	bp.persnr,
+	bp.personid,			
+	kp.fornamn,			
+	kp.tilltalsnamn,		
+	kp.efternamn,           	
+	kp.mellannamn,		
+	kp.aviseringsnamn,		
+	kp.fastighet,			
+	kp.avregistreringsorsak,	
+	kp.fblkf,			
+	kp.kyrkolkf,			
+	kp.kyrkotillhorighetsdatum,	
+	kp.medlemstyp,		
+	kp.andraddatum,		
+        kp.skapaddatum,
+        kp.civilstand,
+        kp.civilstandsdatum,
+	kp.avregisteringsdatum
+INTO	p_arskyddad,			        
+        p_persnr,
+        p_personid,			
+        p_fornamn,			
+        p_tilltalsnamn,		        
+        p_efternamn,           	        
+        p_mellannamn,		        
+        p_aviseringsnamn,		
+        p_fastighet,			
+        p_avregistreringsorsak,	        
+        p_fblkf,			        
+        p_kyrkolkf,			
+        p_kyrkotillhorighetsdatum,	
+        p_medlemstyp,		        
+        p_andraddatum,
+        p_skapaddatum,
+        p_civilstand,		--nytt 20040402 HW
+        p_civilstandsdatum,     --nytt 20040402 HW
+        p_avregisteringsdatum   --nytt 20050215 Perre
+FROM	tbasperson bp,
+	tkyrkoperson kp
+WHERE	bp.personid = kp.personid
+AND	bp.persnr = p_persnr;
+
+IF(p_tilltalsnamn is NULL) THEN 
+	LET p_tilltalsnamn = " "; 
+END IF;
+IF(p_fornamn is NULL) THEN 
+	LET p_fornamn = " "; 
+END IF;
+IF(p_efternamn is NULL) THEN 
+	LET p_efternamn = " "; 
+END IF;
+IF(p_fastighet is NULL) THEN 
+	LET p_fastighet = " "; 
+END IF;
+IF(p_mellannamn is NULL) THEN 
+	LET p_mellannamn = " "; 
+END IF;
+IF(p_aviseringsnamn is NULL) THEN 
+	LET p_aviseringsnamn = " ";
+END IF;
+IF(p_avregistreringsorsak is NULL) THEN 
+	LET p_avregistreringsorsak = " "; 
+END IF;
+IF(p_kyrkotillhorighetsdatum is NULL) THEN 
+	LET p_kyrkotillhorighetsdatum = " ";
+END IF;
+IF(p_fblkf is NULL) THEN 
+	LET p_fblkf = " ";
+END IF;
+IF(p_kyrkolkf is NULL) THEN 
+	LET p_kyrkolkf = " "; 
+END IF;
+
+IF ( (DBINFO("SQLCA.sqlerrd2") != 1) OR (p_avregistreringsorsak='GN') ) THEN 
+	RETURN	0, --Ingen träff i fetchpersonnavet. 
+		" ",		--2 
+		p_persnr_ut,    --3 
+		" ",    	--4 
+		" ",    	--5 
+		" ",    	--6 
+		" ",    	--7 
+		" ",    	--8 
+		" ",    	--9 
+		" ",    	--10
+		" ",    	--11
+		" ",    	--12
+		" ",   		--13
+		" ",    	--14
+		" ",    	--15
+		" ",    	--16
+		" ",    	--17
+		" ",    	--18
+		" ",    	--19
+		" ",    	--20
+		" ",    	--21
+		" ",    	--22
+		" ",  		--23
+		" ",		--24
+		NULL,		--25
+		"E",		--26         
+		NULL;		--27
+
+ELIF (p_arskyddad = 'J') THEN
+	RETURN	p_svar,		--1
+		p_arskyddad,	--2 	  
+		p_persnr,    	--3       
+		" ",    	--4       
+		" ",    	--5       
+		" ",    	--6       
+		" ",    	--7       
+		" ",    	--8       
+		" ",    	--9       
+		" ",    	--10      
+		" ",    	--11      
+		" ",    	--12      
+		" ",   		--13      
+		" ",    	--14      
+		" ",    	--15      
+		" ",    	--16      
+		" ",    	--17      
+		" ",    	--18      
+		" ",    	--19      
+		" ",    	--20      
+		" ",    	--21      
+		" ",    	--22      
+		" ",  		--23
+		" ",		--24
+		NULL,		--25
+		"S",		--26      	
+		NULL;		--27
+
+ELIF (p_avregistreringsorsak='AV') THEN           
+	
+	IF p_andraddatum IS NULL THEN
+		LET p_andraddatum = p_skapaddatum;
+	END IF;	
+	
+	RETURN	p_svar,			        --1     
+		p_arskyddad,		        --2 	
+		p_persnr,			--3     
+		p_fornamn,		        --4             
+		p_tilltalsnamn,		        --5     
+		p_efternamn,           	        --6     
+		p_mellannamn,		        --7     
+		p_aviseringsnamn,		--8     
+		p_fastighet,		        --9     
+		p_avregistreringsorsak,	        --10    
+		p_fblkf,		        --11    
+		p_kyrkolkf,		        --12    
+		p_kyrkotillhorighetsdatum,      --13    
+		p_medlemstyp,		        --14    
+		p_andraddatum,		        --15    
+		" ",                    	--16    
+		" ",                     	--17    
+		" ",                      	--18    
+		" ",                      	--19    
+		" ",                      	--20    
+		" ",                   		--21    
+		" ",                      	--22    
+		" ",                         	--23
+		" ",				--24
+		NULL,				--25
+		"T",				--26         
+		p_avregisteringsdatum;		--27
+ELSE
+	
+	IF (p_avregistreringsorsak = 'PA') THEN           
+		LET p_avregistreringsorsak = " ";		
+	ELIF p_andraddatum IS NULL THEN
+		LET p_andraddatum = p_skapaddatum;
+	END IF;	
+
+	FOREACH
+		SELECT	FIRST 3 
+			a.coadress, 
+		        a.adress1,
+		        a.adress2,
+		        a.adress3,
+		        a.postnummer,
+		        a.postort,
+		        a.land,	        
+		        a.adresstyp,
+		        k.sortnr	        
+		INTO	p_coadress,   	
+		        p_adress1,    
+			p_adress2,    
+		        p_adress3,    
+		        p_postnummer, 
+		        p_postort,    
+		        p_land,       
+		        p_adresstyp,
+		        p_sortnr	        
+		FROM    tadress a,
+			tkoder k
+		WHERE	a.personid = p_personid
+		AND 	a.adresstyp = k.kod 	
+		AND 	k.kodtyp = 'PERSADR'
+		AND 	k.kod != 'T1'
+		ORDER BY k.sortnr
+
+		IF(p_coadress is NULL OR p_coadress= '') THEN 
+			LET p_coadress = " "; 
+		END IF;	
+		IF(p_adress1 is NULL OR p_adress1 = '') THEN 
+			LET p_adress1 = " "; 
+		END IF;	
+		IF(p_adress2 is NULL) THEN 
+			LET p_adress2 = " "; 
+		END IF;	
+		IF(p_adress3 is NULL OR p_adress3 = '') THEN 
+			LET p_adress3 = " "; 
+		END IF;	
+		IF(p_postnummer is NULL) THEN 
+			LET p_postnummer = " ";
+		END IF;	
+		IF(p_postort is NULL) 
+			THEN LET p_postort = " "; 		
+		END IF;
+		IF(p_land is NULL OR p_land ='') THEN 
+			LET p_land = " "; 
+		END IF;
+		LET p_kontroll = 1;
+
+	RETURN	p_svar,			        --1     
+		p_arskyddad,		        --2 	
+		p_persnr,			--3     
+		p_fornamn,		        --4             
+		p_tilltalsnamn,		        --5     
+		p_efternamn,           	        --6     
+		p_mellannamn,		        --7     
+		p_aviseringsnamn,		--8     
+		p_fastighet,		        --9     
+		p_avregistreringsorsak,	        --10    
+		p_fblkf,		        --11    
+		p_kyrkolkf,		        --12    
+		p_kyrkotillhorighetsdatum,      --13    
+		p_medlemstyp,		        --14    
+		p_andraddatum,		        --15    
+		p_adresstyp,                    --16    
+		p_coadress,                     --17    
+		p_adress1,                      --18    
+		p_adress2,                      --19    
+		p_adress3,                      --20    
+		p_postnummer,                   --21    
+		p_postort,                      --22    
+		p_land, 			--23
+		p_civilstand,		   	--24
+		p_civilstandsdatum,		--25
+		"T",				--26
+		p_avregisteringsdatum  		--27
+			WITH resume;
+	
+	END FOREACH;
+	IF ( p_kontroll = 0 ) THEN
+
+	RETURN	p_svar,			        --1     
+		p_arskyddad,		        --2 	
+		p_persnr,			--3     
+		p_fornamn,		        --4             
+		p_tilltalsnamn,		        --5     
+		p_efternamn,           	        --6     
+		p_mellannamn,		        --7     
+		p_aviseringsnamn,		--8     
+		p_fastighet,		        --9     
+		p_avregistreringsorsak,	        --10    
+		p_fblkf,		        --11    
+		p_kyrkolkf,		        --12    
+		p_kyrkotillhorighetsdatum,      --13    
+		p_medlemstyp,		        --14    
+		p_andraddatum,		        --15    
+		" ",				--16    
+		" ",				--17    
+		" ",				--18    
+		" ",				--19    
+		" ",				--20    
+		" ",				--21    
+		" ",				--22    
+		" ",				--23
+		p_civilstand,		   	--24
+		p_civilstandsdatum,		--25
+		"T",				--26
+		p_avregisteringsdatum; 		--27
+
+	END IF;
+
+	
+END IF;                         
+        	
+-- $Log: fetchpersonnavet.sql,v $
+-- Revision 1.1  2006/08/24 08:56:34  ovew
+-- knavet1.2 051220
+--
+END PROCEDURE;
---
+++ login_knavet.sql
@@ -0,0 +1,179 @@
+DROP PROCEDURE login_knavet;
+CREATE PROCEDURE login_knavet(p_loginnamn char(10),p_behoriguppg char(1),p_rutin char(10),p_enhetsid INT,p_losenord varchar(32,1),p_system varchar(10))
+	returning 
+	int, 
+	varchar(255,0),
+	int,
+	char(12),
+	char(60),
+	char(12),
+	char(1),
+	datetime year to fraction(3),
+	datetime year to fraction(3),
+	int,
+	datetime year to fraction(3); --Sessionsid
+
+--@(#)$Id: login_knavet.sql,v 1.1 2006/08/24 08:56:39 ovew Exp $
+
+
+{
+-- Skapat av: Susanna Lindkvist
+-- Datum: 2002-11-20
+-- Ändrat: Perre Forsberg 2003-01-10
+-- Ändrat: Perre Forsberg 2003-09-17
+-- Version: 1
+-- Rutinbeteckning ??
+-- Kontrollerar login namn/lösenord, ej sessionshantering.
+}
+
+DEFINE p_svar integer;
+
+
+DEFINE p_appanvappid char(18);
+DEFINE p_anvandareid int;
+DEFINE p_anvandarenamn char(60);
+DEFINE p_anvandarepersnr char(12);
+DEFINE p_araktiv char(1);
+DEFINE p_behorighetslista varchar(255,0);
+DEFINE p_behorighetslistahelp1 varchar(255,0);
+DEFINE p_behorighetslistahelp2 varchar(255,0);
+DEFINE p_senastindat datetime year to fraction(3);
+DEFINE p_senastutdat datetime year to fraction(3);
+DEFINE p_antaltillhor int;
+DEFINE 	p_inloggad char(1); 	
+DEFINE	p_sessionsid datetime year to fraction(3);	
+DEFINE	p_senasteaktivitet datetime year to fraction(3);
+DEFINE p_sessiontimeouttime     INTERVAL MINUTE TO MINUTE; 
+DEFINE p_userid char(10);
+DEFINE p_password varchar(32,1);
+DEFINE p_passcheck int;
+DEFINE p_antal integer;
+
+DEFINE sql_err integer;
+DEFINE isam_err integer;
+DEFINE error_info char(70);
+
+
+-- Felrutin
+
+ON EXCEPTION SET sql_err, isam_err, error_info
+	CALL error_log(sql_err, isam_err, error_info,p_loginnamn,'1001');
+	RETURN 1001,null,null,null,null,null,null,null,null,null,null;
+	RAISE EXCEPTION sql_err, isam_err, error_info;	
+END EXCEPTION;
+SET LOCK MODE TO WAIT 10;
+
+LET p_svar = 0;
+LET p_behorighetslista = " ";
+LET p_enhetsid = 0;
+LET p_anvandarenamn = "";
+LET p_anvandarepersnr = "";
+LET p_araktiv = "";
+LET p_senastindat = null;
+LET p_senastutdat = null;
+LET p_antaltillhor = 0;
+LET p_sessiontimeouttime = "20";
+LET p_inloggad = NULL;
+LET p_sessionsid = CURRENT;
+LET p_userid = NULL;
+
+execute procedure check_userpasswd(p_loginnamn,p_losenord)
+into p_passcheck;
+
+IF(p_passcheck != 0) THEN -- Fel i passwdkontrollen.
+	return p_passcheck,null,null,null,null,null,null,null,null,null,null;
+END IF;
+
+-- Om fel lösenord returnera 1003
+
+SELECT 	inloggad,
+	senasteaktivitet,
+	senasteinloggadenhetid,
+	senasteinloggaddatum,	
+	senasteutloggaddatum	
+INTO	p_inloggad, 	
+	p_senasteaktivitet,
+	p_enhetsid,
+	p_senastindat,
+	p_senastutdat  
+FROM 	tanvappdata_ny
+WHERE	userid = p_loginnamn;
+
+--IF (p_inloggad is not NULL) THEN
+--IF (p_inloggad='J' AND p_sessionsid - p_senasteaktivitet < p_sessiontimeouttime) THEN
+--	return 1002,null,null,null,null,null,null,null,null,null,null; --Användare inloggad och aktiv.
+--ELIF (p_inloggad='J' AND p_sessionsid - p_senasteaktivitet >= p_sessiontimeouttime) THEN
+--	LET p_senastutdat = p_senasteaktivitet + p_sessiontimeouttime;
+--END IF;
+--END IF;
+
+
+SELECT	p.anamn,
+	a.persnr,
+	a.araktiv,
+	a.userid
+INTO	p_anvandarenamn,
+	p_anvandarepersnr,
+	p_araktiv,
+	p_userid
+FROM	tanvandare_ny a, torgperson p
+WHERE	a.userid = p_loginnamn
+and	a.persnr = p.persnr;
+
+--Ingen träff
+IF (p_userid is null) THEN
+	RETURN 1004,null,null,null,null,null,null,null,null,null,null; -- Användaren finns ej.
+ELSE	
+
+Select count(*)   
+into p_antal
+from tanvbehorig b, tanvkat a, tanvkatroll ar, troll r, trollegenskap re, tegenskap e, tegenskapsrutin er
+where b.userid = p_loginnamn
+and b.anvkatid = a.anvkatid
+and a.anvkatid = ar.anvkatid
+and ar.rollid = r.rollid
+and r.rollid = re.rollid
+and re.egenskapsid = e.egenskapsid
+and e.egenskapsid = er.egenskapsid
+and er.rutinid = 400;
+
+IF(p_antal < 1) then
+	RETURN 1004,null,null,null,null,null,null,null,null,null,null; -- Användaren finns ej.
+end if;   
+
+
+	IF (p_behoriguppg = 'J' OR p_behoriguppg = 'j') THEN
+		EXECUTE PROCEDURE getbehorighetallman(p_userid,p_system)
+		into p_svar,p_behorighetslista;
+--		EXECUTE PROCEDURE getbehorighetenhet(p_userid,p_system,0)
+--		into p_svar,p_behorighetslistahelp1;
+--		EXECUTE PROCEDURE getbehorighetverksamhet(p_userid,p_system,0)
+--		into p_svar,p_behorighetslistahelp2;
+		
+--		LET p_behorighetslista = p_behorighetslista || p_behorighetslistahelp1 || p_behorighetslistahelp2;
+	ELSE
+		LET p_behorighetslista = NULL;
+	END IF;
+
+
+
+	RETURN 
+	p_svar,
+	p_behorighetslista,
+	p_enhetsid,
+	p_loginnamn,
+        p_anvandarenamn,
+	p_anvandarepersnr,
+	p_araktiv,
+	p_senastindat,
+	p_senastutdat,
+	p_antaltillhor, 
+	p_sessionsid;
+END IF;
+
+
+-- $Log: login_knavet.sql,v $
+-- Revision 1.1  2006/08/24 08:56:39  ovew
+-- knavet1.2 051220
+--
+END PROCEDURE;
